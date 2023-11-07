5cd93a97edee416dd901668a9de9e136d76d2d14
unknown <philippediniz@outlook.com>
2023-11-07 16:46:03 -0300

    first commit

---
+++ src/main/java/com/controleDeAcesso/ControleDeAcesso/ControleDeAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.controleDeAcesso.ControleDeAcesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controleDeAcesso/ControleDeAcesso/ControleDeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleDeAcesso.ControleDeAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
